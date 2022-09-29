dd512c05d9139038590b25d1c187d887caf6f8a0
cem <cemsimseksm@gmail.com>
2022-09-30 02:37:27 +0300

    html structure created

---
+++ Dice Game/index.js
@@ -0,0 +1,5 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random number between 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+
+document.querySelector("img1");
