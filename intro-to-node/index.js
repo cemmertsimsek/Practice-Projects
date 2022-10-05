78a55f855bd9b457509c4fc928d1c7351978964e
cem <cemsimseksm@gmail.com>
2022-10-05 17:00:49 +0300

    random name generator installed via npm

---
+++ intro-to-node/index.js
@@ -0,0 +1,11 @@
+const supervillains = require("supervillains");
+const superheroes = require("superheroes");
+
+superheroes.all;
+supervillains.all;
+
+var mySuperHeroes = superheroes.random();
+var mySuperVillains = supervillains.random();
+
+console.log(mySuperHeroes);
+console.log(mySuperVillains);
