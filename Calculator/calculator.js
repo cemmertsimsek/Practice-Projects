2f22730a169e0b4d1f76632fecf37273b59050a8
cem <cemsimseksm@gmail.com>
2022-10-06 19:19:33 +0300

    bmi calculator finished

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,18 +6,19 @@ const bodyParser = require("body-parser"); // activating the bodyparser package 
 const app = express(); //app is for using express() function everytime
 app.use(bodyParser.urlencoded({ extended: true })); // format of using bodyparser with encoded url. extended: true is not optional.
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html"); //using .sendFile for sending file such as index.html
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html"); //using .sendFile for sending file such as index.html
 }); // we get the full path of the directories location and added index.html for fullpath
 
-app.post("/", function (req, res) {
+app.post("/bmicalculator", function (req, res) {
   // we post the values, parse it and use the values for making calculations
-  var num1 = Number(req.body.num1); //req.body is all the information we get
-  var num2 = Number(req.body.num2); //req.body.num2 is the inputs name which given by us in the html file
-  // because req.body.num2 is a string format we need to change its type with Number() method to calculate 2 integers
-  var result = num1 + num2; //result is for plus equasion only for this practise
+  var weight = parseFloat(req.body.weight); //req.body is all the information we get
+  var height = parseFloat(req.body.height); //req.body.num2 is the inputs name which given by us in the html file
+  // because req.body.num2 is a string format we need to change its type with Number() method to calculate 2 integers parseFloat() is also usable
+  // beacuse req.body.weight
+  var result = weight / (height * height); //result is for plus equasion only for this practise
 
-  res.send("The result of the calculation is " + result); //posting the result we get with the string
+  res.send("Your BMI is: " + result); //posting the result we get with the string
 });
 
 app.listen(3000, function () {
