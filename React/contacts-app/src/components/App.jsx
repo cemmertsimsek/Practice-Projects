3f41bbb2cba410d4e0ed471e4aad8b4e196101f4
cem <cemsimseksm@gmail.com>
2022-11-04 16:58:46 +0300

    map function used

--- React/contacts-app/src/components/App.jsx
+++ React/contacts-app/src/components/App.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contact";
+import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4D03AQF7btwal_PLpg/profile-displayphoto-shrink_400_400/0/1645706165473?e=1671062400&v=beta&t=ifmhlY5WmyOTJdCEDWRlxo8f2geyg1aofGT91XF3iM4" />
+
+      {contacts.map(createCard)}
+
+      {/* <Card
         name={contacts[0].name}
         tel={contacts[0].phone}
         email={contacts[0].email}
@@ -23,7 +41,7 @@ function App() {
         tel={contacts[2].phone}
         email={contacts[2].email}
         img={contacts[2].imgURL}
-      />
+      /> */}
     </div>
   );
 }
