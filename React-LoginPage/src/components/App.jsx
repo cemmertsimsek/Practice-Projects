82b4438286447f1d33c9c49fc650df72d39d4a46
cem <cemsimseksm@gmail.com>
2022-11-07 17:25:53 +0300

    ternary, and operations explained

--- React-LoginPage/src/components/App.jsx
+++ React-LoginPage/src/components/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date(2022, 11, 1, 9).getHours();
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {
+        //TERNARY OPERATOR
+        //isLoggedIn ? <h1>Hello</h1> : <Login />
+        //currentTime > 12 ? <h1>Why are you still working?</h1> : null
+        //&& OPERATOR
+        //currentTime > 12 && <h1>Why are you still working?</h1>
+        //We can use this method for not checking both condition is true. but we know if the first condition is false JS is not going for the expression
+      }
     </div>
   );
 }
